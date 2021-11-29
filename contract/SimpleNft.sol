ddc97f5bb44eeab2998fad1de86a3632cd34cf49
Jaime Serrano gonzalez <jserrano@c3r10s5.42madrid.com>
2021-11-29 15:11:36 +0100

    gestionar repeticion de minteos

--- contract/SimpleNft.sol
+++ contract/SimpleNft.sol
@@ -2,15 +2,15 @@
 
 // Amended by HashLips
 /**
-    !Disclaimer!
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    HashLips will not be liable in any way if for the use 
-    of the code. That being said, the code has been tested 
-    to the best of the developers' knowledge to work as intended.
+		!Disclaimer!
+		These contracts have been used to create tutorials,
+		and was created for the purpose to teach people
+		how to create smart contracts on the blockchain.
+		please review this code on your own before using any of
+		the following code for production.
+		HashLips will not be liable in any way if for the use
+		of the code. That being said, the code has been tested
+		to the best of the developers' knowledge to work as intended.
 */
 
 pragma solidity >=0.7.0 <0.9.0;
@@ -19,126 +19,149 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
+	using Strings for uint256;
 
-  string baseURI;
-  string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
-  uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
-  bool public revealed = false;
-  string public notRevealedUri;
+	string baseURI;
+	string public baseExtension = ".json";
+	uint256 public cost = 0.05 ether;
+	uint256 public currentSupply;
+	uint256 public maxSupply;
+	uint256 public maxMintAmount = 20;
+	bool public paused = false;
+	bool public revealed = false;
+	string public notRevealedUri;
+	uint256[] tokensAssigned;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    string memory _initNotRevealedUri
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
-  }
+	constructor(
+		string memory _name,
+		string memory _symbol,
+		string memory _initBaseURI,
+		string memory _initNotRevealedUri,
+		uint256 _initialSupply,
+		uint256 _maxSupply
+	) ERC721(_name, _symbol) {
+		setBaseURI(_initBaseURI);
+		setNotRevealedURI(_initNotRevealedUri);
+		currentSupply = _intialSupply;
+		maxSupply = _maxSupply;
+	}
 
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
+	// internal
+	function _baseURI() internal view virtual override returns (string memory) {
+		return baseURI;
+	}
 
-  // public
-  function mint(uint256 _mintAmount) public payable {
-    uint256 supply = totalSupply();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(supply + _mintAmount <= maxSupply);
+	function random(uint256 range) internal view returns (uint256) {
+		return uint256(keccak256(block.timestamp, block.difficulty, msg.sender)) % range;
+	}
 
-    if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount);
-    }
+	// public
+	function setMaxSupply(uint256 n) public onlyOwner{
+		require(totalSupply() + n <= maxSupply, "No somos bolivarianos!");
+		currentSupply = n;
+	}
 
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, supply + i);
-    }
-  }
+	function mint(uint256 _mintAmount) public payable {
+		uint256 supply = totalSupply();
+		uint256 rnd_num;
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-    }
-    return tokenIds;
-  }
+		require(!paused);
+		require(_mintAmount > 0);
+		//require(_mintAmount <= maxMintAmount);
+		require(supply + _mintAmount <= maxSupply);
 
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
-    
-    if(revealed == false) {
-        return notRevealedUri;
-    }
+		if (msg.sender != owner()) {
+			require(msg.value >= cost * _mintAmount);
+		}
 
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
+		for (uint256 i = 1; i <= _mintAmount; i++) {
+			rnd_num = random();
+			for (uint256 i = rnd_num + 1; i != rnd_num; i++){
+				if (i == tokensAssigned.length)
+					i = 0;
+				if ()
+			}
+			_safeMint(msg.sender, supply + i);
+		}
+	}
 
-  //only owner
-  function reveal() public onlyOwner {
-      revealed = true;
-  }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
-  }
+	function walletOfOwner(address _owner)
+		public
+		view
+		returns (uint256[] memory)
+	{
+		uint256 ownerTokenCount = balanceOf(_owner);
+		uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+		for (uint256 i; i < ownerTokenCount; i++) {
+			tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+		}
+		return tokenIds;
+	}
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
-  }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
+	function tokenURI(uint256 tokenId)
+		public
+		view
+		virtual
+		override
+		returns (string memory)
+	{
+		require(
+			_exists(tokenId),
+			"ERC721Metadata: URI query for nonexistent token"
+		);
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
+		if(revealed == false) {
+				return notRevealedUri;
+		}
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
+		string memory currentBaseURI = _baseURI();
+		return bytes(currentBaseURI).length > 0
+				? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+				: "";
+	}
 
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
-  }
- 
-  function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }
+	//only owner
+	function reveal() public onlyOwner {
+			revealed = true;
+	}
+
+	function setCost(uint256 _newCost) public onlyOwner {
+		cost = _newCost;
+	}
+
+	function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+		maxMintAmount = _newmaxMintAmount;
+	}
+
+	function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+		notRevealedUri = _notRevealedURI;
+	}
+
+	function setBaseURI(string memory _newBaseURI) public onlyOwner {
+		baseURI = _newBaseURI;
+	}
+
+	function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+		baseExtension = _newBaseExtension;
+	}
+
+	function pause(bool _state) public onlyOwner {
+		paused = _state;
+	}
+
+	function withdraw() public payable onlyOwner {
+		// This will pay HashLips 5% of the initial sale.
+		// You can remove this if you want, or keep it in to support HashLips and his channel.
+		// =============================================================================
+		(bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
+		require(hs);
+		// =============================================================================
+
+		// This will payout the owner 95% of the contract balance.
+		// Do not remove this otherwise you will not be able to withdraw the funds.
+		// =============================================================================
+		(bool os, ) = payable(owner()).call{value: address(this).balance}("");
+		require(os);
+		// =============================================================================
+	}
 }
